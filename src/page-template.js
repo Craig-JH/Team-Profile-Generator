b122e097df40383ab27568e661efb0594800036a
Craig Hyman <misccorp@yahoo.com>
2022-08-27 09:50:36 -0400

    updated index.html and page-template to fix icon issues

--- src/page-template.js
+++ src/page-template.js
@@ -8,7 +8,7 @@ function generateHTML(teamArray) {
                               <div class="card-body px-0 py-0">
                                   <div class="card-header header px-3">
                                       <h5 class="card-title font-weight-bold">${employee.name}</h5>
-                                      <h6 class="card-subtitle font-weight-bold"><span class = "icon mr-1"><i class="fa-regular fa-handshake"></i></span>Manager</h6>
+                                      <h6 class="card-subtitle font-weight-bold"><span class = "icon mr-1"><i class="fa-solid fa-list-check"></i></span>Manager</h6>
                                   </div>
                                   <ul class="card-text list-group mt-2 px-3 py-3 list">
                                       <li id="ID list-group-item">ID: ${employee.id}</li>
@@ -22,7 +22,7 @@ function generateHTML(teamArray) {
             <div class="card-body px-0 py-0">
                 <div class="card-header header px-3">
                     <h5 class="card-title font-weight-bold">${employee.name}</h5>
-                    <h6 class="card-subtitle font-weight-bold"><span class = "icon mr-1"><i class="fa-solid fa-file-pen"></i></span>Intern</h6>
+                    <h6 class="card-subtitle font-weight-bold"><span class = "icon mr-1"><i class="fa-solid fa-user-graduate"></i></span>Intern</h6>
                 </div>
                 <ul class="card-text list-group mt-2 px-3 py-3 list">
                     <li id="ID list-group-item">ID: ${employee.id}</li>
@@ -32,7 +32,7 @@ function generateHTML(teamArray) {
             </div>
         </div>`;
     } else {
-      cardSection += `           <div class="col-9-sm col-6-md col-lg-3 card shadow p-0 mb-5 bg-light rounded mx-4">
+      cardSection += `  <div class="col-9-sm col-6-md col-lg-3 card shadow p-0 mb-5 bg-light rounded mx-4">
             <div class="card-body px-0 py-0">
                 <div class="card-header header px-3">
                     <h5 class="card-title font-weight-bold">${employee.name}</h5>
@@ -57,13 +57,13 @@ function generateHTML(teamArray) {
       <title>Team Profile</title>
       <script src="https://kit.fontawesome.com/e7569cb0c8.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <link rel="stylesheet" href="styles.css">
     </head>
 
     <header>
-        <h1 class="title text-left ml-5 py-2 font-weight-bolder"> <span class="icon" id="header-icon"><i class="fa-regular fa-address-card"></i></span> My Team</h1>
+        <h1 class="title text-left ml-5 py-2 font-weight-bolder"> <span class="icon" id="header-icon"><i class="fa-solid fa-people-group"></span> My Team</h1>
     </header>
 
     <main class="container mt-5">
